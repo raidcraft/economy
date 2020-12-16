a3ae219e3232e4f551d5b5a5c96bce4eb943e291
Silthus <michael@reichenbach.in>
2020-12-16 16:14:29 +0100

    refactor: split core and wrapper into separate modules

--- wrapper/src/main/java/de/raidcraft/economy/Economy.java
+++ wrapper/src/main/java/de/raidcraft/economy/Economy.java
@@ -52,11 +52,28 @@ public class Economy implements net.milkbowl.vault.economy.Economy {
         this.economy = rcEconomy;
     }
 
+    /**
+     * Checks if the RCEconomy plugin exists and is enabled.
+     * <p>This wrapper will automatically fallback to the default Vault implementation
+     * if the RCEconomy plugin is not found.
+     *
+     * @return true if RCEconomy exists and is enabled
+     */
     public boolean isRcEconomyEnabled() {
 
         return rcEconomy != null;
     }
 
+    /**
+     * Withdraws the given amount from the player and logs the transaction with the given details.
+     * <p>Use this to show rich information to players when they execute the <pre>/transactions</pre> command.
+     * <p>The details are silently omitted if {@link #isRcEconomyEnabled()} is false.
+     *
+     * @param player the player to withdraw money from
+     * @param amount the amount to withdraw
+     * @param details the details to log into the transaction log
+     * @return the result of the transaction
+     */
     public EconomyResponse withdrawPlayer(OfflinePlayer player, double amount, String details) {
 
         if (isRcEconomyEnabled()) {
